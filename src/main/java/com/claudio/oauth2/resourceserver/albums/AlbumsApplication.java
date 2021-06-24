0954649236e4cbc2e0c6c8dede6263406d28b817
Claudio Cifuentes <cacifuentesalonso@gmail.com>
2021-06-24 17:45:12 -0500

    Application code

---
+++ src/main/java/com/claudio/oauth2/resourceserver/albums/AlbumsApplication.java
@@ -0,0 +1,15 @@
+package com.claudio.oauth2.resourceserver.albums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AlbumsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumsApplication.class, args);
+	}
+
+}
